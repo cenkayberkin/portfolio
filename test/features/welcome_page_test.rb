626088c867693b1afdb910a27ea80a8cd6f49ddd
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-16 16:14:52 -0700

    Welcome controller and page added, testing its content

--- test/features/welcome_page_test.rb
+++ test/features/welcome_page_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 feature "WelcomePage" do
   scenario "Checking content of welcome page" do
-    visit root_path
+    visit 'http://localhost:3000/'
     page.must_have_content "Welcome"
     page.wont_have_content "No no no"
   end
