feefe77b89562b81df3d93f6c95728497985365b
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-22 16:27:00 -0700

    Porject model and controller added

--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
-class Projects::CreatingAProjectTest < Capybara::Rails::TestCase
-  test "sanity" do
-    visit root_path
-    assert_content page, "Hello World"
-    refute_content page, "Goobye All!"
+feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
+  scenario "adding a new project" do
+    visit projects_path
+    click_on "New Project"
+    fill_in "Name", with: "Code Fellows Portfolio"
+    fill_in "Technologies used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
+    click_on "Create Project"
+    page.text.must_include "Project has been created"
+    assert page.has_css?("#notice"), "Expected a flash notice on this page, none found."
+    page.status_code.must_equal 200
   end
 end
