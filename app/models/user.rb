f2b332fb553ab32fe61cb13d976677ea346ee7b8
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-30 16:10:51 -0700

    All tests are passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      # user.name = auth.info.nickname
       user.email = "#{auth.info.nickname}-CHANGEME@twitter.example.com"
     end
   end
--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -16,18 +16,22 @@ feature("
   end
 
   scenario "sign in with twitter works" do
-   visit root_path
-   click_on "Sign In"
+
+   # save_and_open_page
+   # click_on "Sign In"
    OmniAuth.config.test_mode = true
-   Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
-   Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
    OmniAuth.config.add_mock(:twitter,
                             {
                             uid: '12345',
                             info: { nickname: 'test_twitter_user'},
                             })
+
+   visit root_path
+
+   Capybara.current_session.driver.request.env['devise.mapping'] = Devise.mappings[:user]
+   Capybara.current_session.driver.request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+
    click_on "Sign in with Twitter"
-   save_and_open_page
-   page.must_have_content "test_twitter_user, you are signed in!"
+   page.must_have_content "Logged in as test_twitter_user"
   end
 end
