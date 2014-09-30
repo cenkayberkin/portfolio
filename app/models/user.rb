3d964cddb2859fe73bf1fd2dac0cfd1545018257
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-30 11:29:18 -0700

    mass assignment problem solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,11 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.info.nickname
-      user.email = "#{user.name}-CHANGEME@twitter.example.com"
+      # user.name = auth.info.nickname
+      user.email = "#{auth.info.nickname}-CHANGEME@twitter.example.com"
     end
   end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :twitter, ENV["consumer_key"], ENV["consumer_secret"]
+  config.omniauth :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
