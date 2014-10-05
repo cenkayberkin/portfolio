77cefeb7873f99f5388dba5728f366cb4b2b5076
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-10-05 13:17:48 -0700

    Tests are passing, articles scope updated, projects and articles comment are working

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   has_many :comments, as: :commentable
+  scope :published, -> { where(published: true) }
 
   def publish!
     published = true
--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -17,4 +17,17 @@ class ArticlePolicy
   def destroy?
     @user.editor?
   end
+
+  Scope = Struct.new(:user, :scope) do
+    def resolve
+      if user.present? && user.editor?
+        scope.all
+      elsif user.present? && user.author?
+        scope.where(author_id: user.id) | scope.published
+      else
+        scope.where(published: true)
+      end
+    end
+  end
+
 end
